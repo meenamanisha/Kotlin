4b954099f6640ec60c83fc6861ea352992201ab3
Manisha Meena <mnime@amazon.com>
2020-04-14 16:10:32 +0530

    Check String usage
    Check String usage test
    
    test
    
    hello
    
    Check String usage
    
    Check String usage test

--- src/practice/Hello.kt
+++ src/practice/Hello.kt
@@ -5,5 +5,12 @@ fun main(args: Array<String>)
 	println("Hello World!!")
 	var a:Int = 10
 	println(a)
+
+	var rawString :String  = "I am Raw String!"
+	val escapedString : String  = "I am escaped String!\n"
+
+	println("Hello!"+escapedString)
+	println("Hey!!"+rawString)
+	print("jhdkdusyfiduyfiduyf")
 }
 
