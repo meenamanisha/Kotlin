5c578bfc3c9dff917714ad3a2d492e4ea5faa7a3
Manisha Meena <mnime@amazon.com>
2020-08-17 19:10:03 +0530

    Revert "Check String usage"
    This reverts commit 4b954099f6640ec60c83fc6861ea352992201ab3.

--- src/practice/Hello.kt
+++ src/practice/Hello.kt
@@ -5,12 +5,5 @@ fun main(args: Array<String>)
 	println("Hello World!!")
 	var a:Int = 10
 	println(a)
-
-	var rawString :String  = "I am Raw String!"
-	val escapedString : String  = "I am escaped String!\n"
-
-	println("Hello!"+escapedString)
-	println("Hey!!"+rawString)
-	print("jhdkdusyfiduyfiduyf")
 }
 
